3d8595eef6eaf25770d8d63fafe309bd1d4f6827
Abhimanyu Yadav <abhimanyu@headerlabs.com>
2015-08-02 12:03:27 +0530

    create order api and a few minor chnages

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -2,53 +2,54 @@
 
 class orderController extends \BaseController {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-	   $order=new Order();
-           $ordersDetails=$order->getDetailedOrders();
-           $msg="Orders Details are retrieved successfully.";
-           return $this->successMessageWithVar($msg, $ordersDetails,'detailedOrders'); 
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index() {
+        $order = new Order();
+        $ordersDetails = $order->getDetailedOrders();
+        $msg = "Orders Details are retrieved successfully.";
+        return $this->successMessageWithVar($msg, $ordersDetails, 'detailedOrders');
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-	print_r(Input::all());
-        exit;
-	}
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        $order = new Order();
+        $orderInfo=$order->saveOrder();
+        $order->saveOrderItems($orderInfo->id);
+        $order->saveShippingAddress($orderInfo->id);
+        $orderDetail = $order->getOrderById($orderInfo->id);
+        $msg = "Order Details are saved successfully.";
+        return $this->successMessageWithVar($msg, $orderDetail, 'orderDetails');
+    }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        $order = new Order();
+        $orderDetail = $order->getOrderById($id);
+        $msg = "Order Details are retrieved successfully.";
+        return $this->successMessageWithVar($msg, $orderDetail, 'orderDetails');
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-            $order=new Order();
-            $orderDetail=$order->getOrderById($id);
-            $msg="Order Details are retrieved successfully.";
-            return $this->successMessageWithVar($msg, $orderDetail,'orderDetails');            
-	}
-   
-        public function successMessageWithVar($msg,$data,$key){
+    public function successMessageWithVar($msg, $data, $key) {
         return json_encode(array(
-                'success' => true,
-                'messages' => $msg,
-                'response' => array(
-                    $key => $data)
-                ),
-            200
+            'success' => true,
+            'messages' => $msg,
+            'response' => array(
+                $key => $data)
+                ), 200
         );
     }
+
 }
--- app/models/Order.php
+++ app/models/Order.php
@@ -23,21 +23,50 @@ class Order extends Eloquent {
     }
 
     public function getDetailedOrders() {
-        $where=Input::all();
-        $orderIds=DB::table($this->table)->where($where)->lists('id');
-        $ordersDetails=array();
+        $where = Input::all();
+        $orderIds = DB::table($this->table)->where($where)->lists('id');
+        $ordersDetails = array();
         foreach ($orderIds as $orderId) {
-            $orderDetails=$this->getOrderById($orderId);
-            $ordersDetails[]=$orderDetails;
+            $orderDetails = $this->getOrderById($orderId);
+            $ordersDetails[] = $orderDetails;
         }
         return $ordersDetails;
     }
-    
+
     public function getShippingAddressByOrderID($id) {
-                return $orderItems = DB::table('orders')
+        return $orderItems = DB::table('orders')
                         ->join('shipping_addresses as sa', 'sa.order_id', '=', 'orders.id')
                         ->select('sa.*')
                         ->where('order_id', $id)->first();
-        
     }
+
+    public function saveOrder() {
+        $this->user_id = Input::get('user_id');
+        $this->subtotal = Input::get('subtotal');
+        $this->tax = Input::get('tax');
+        $this->discount = Input::get('discount');
+        $this->shipping_amount = Input::get('shipping_amount');
+        $this->charged_amount = Input::get('charged_amount');
+        $this->save();
+        return $this;
+    }
+
+    public function saveOrderItems($orderId) {
+        $orderItems = Input::get('orderItems');
+        foreach ($orderItems as $orderItem) {
+            $orderItem['order_id'] = $orderId;
+            $orderItem['created_at'] = date("Y-m-d H:i:s");
+            $orderItem['updated_at'] = date("Y-m-d H:i:s");
+            DB::table('order_details')->insert($orderItem);
+        }
+    }
+
+    public function saveShippingAddress($orderId) {
+        $shippingAddress = Input::get('shippingAddress');
+        $shippingAddress['order_id'] = $orderId;
+        $shippingAddress['created_at'] = date("Y-m-d H:i:s");
+        $shippingAddress['updated_at'] = date("Y-m-d H:i:s");
+        DB::table('shipping_addresses')->insert($shippingAddress);
+    }
+
 }
